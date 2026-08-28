cb18895da9e06f5cde5837967583dacadfb2b1c7
srianbury <bsunbury29@gmail.com>
2026-08-28 15:06:27 -0700

    closes #45

---
+++ src/tests/Sample/sample.spec.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+
+describe("sample", () => {
+  describe("sample", () => {
+    it("runs the test suite", async () => {
+      // Sample to ensure the tests actually run
+      expect(true).to.eql(true);
+    });
+  });
+});
